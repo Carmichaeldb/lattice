e09bc61ea356761806965d5f0347c4b5188d12fb
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-23 19:14:47 -0800

    add routes for search

--- server.js
+++ server.js
@@ -29,11 +29,12 @@ app.use(express.static('public'));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require('./routes/users');
-
+const searchRoutes = require('./routes/search');
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/users`
 app.use('/users', usersRoutes);
+app.use('/s', searchRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
