c7dfdfbc010158e98dd5dba34946546505121c20
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-23 19:08:23 -0800

    add jQuery and Ajax to get input from search Input and sends post requests

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,21 @@
 // Client facing scripts here
+$(document).ready(function () {
+  // --- our code goes here ---
+  $("#searchId").on("input", function (event) {
+    event.preventDefault();
+    const $searchText = $("#searchId").val();
+    console.log("$searchText:", $searchText);
+    $.ajax({
+      method: 'POST',
+      url: '/',
+      data: $searchText,
+      success: function (res) {
+        console.log(`data received:: ${res}`);
+       // loadTweets();//to load entered tweet as an article
+      },
+      error: function (err) {
+        console.log(`error received:: ${err}`);
+      }
+    });
+  });
+});
