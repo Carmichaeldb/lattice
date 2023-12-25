12bf8bb6ca7853e35c1fccd4a362c891d8b779f7
Roderick Shoolbraid <rdxenv@gmail.com>
2023-12-24 20:13:15 -0800

    working on topics|  query - ejs & routes part 1

--- routes/topics.js
+++ routes/topics.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db/connection'); // Adjust the path to your database connection file
+const db = require('../db/connection');
 
 router.get('/', async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM topics');
-    res.render('topics', { topics: result.rows });
+    // Fetch all topics
+    const topicsResult = await db.query('SELECT * FROM topics');
+
+    // Prepare a map/object to hold topics and their posts
+    const topicsWithPosts = {};
+
+    // For each topic, fetch the associated posts
+    for (const topic of topicsResult.rows) {
+      const postsResult = await db.query('SELECT * FROM posts WHERE topic_id = $1 ORDER BY created_at DESC', [topic.id]);
+      // Add the topic and its posts to the map
+      topicsWithPosts[topic.id] = {
+        ...topic,
+        posts: postsResult.rows
+      };
+    }
+
+    // Pass the organized topics and posts to the EJS template
+    res.render('topics', { topicsWithPosts: Object.values(topicsWithPosts) });
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
