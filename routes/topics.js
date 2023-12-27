77aea9905a5058234a872920c0a21773f358f30d
Roderick Shoolbraid <rdxenv@gmail.com>
2023-12-26 17:10:57 -0800

    user routes-debug

--- routes/topics.js
+++ routes/topics.js
@@ -4,24 +4,22 @@ const db = require('../db/connection');
 
 router.get('/', async (req, res) => {
   try {
-    // Fetch all topics
     const topicsResult = await db.query('SELECT * FROM topics');
-
-    // Prepare a map/object to hold topics and their posts
     const topicsWithPosts = {};
 
-    // For each topic, fetch the associated posts
     for (const topic of topicsResult.rows) {
       const postsResult = await db.query('SELECT * FROM posts WHERE topic_id = $1 ORDER BY created_at DESC', [topic.id]);
-      // Add the topic and its posts to the map
       topicsWithPosts[topic.id] = {
         ...topic,
         posts: postsResult.rows
       };
     }
 
-    // Pass the organized topics and posts to the EJS template
-    res.render('topics', { topicsWithPosts: Object.values(topicsWithPosts) });
+    // Fetch the user object from the session or use a default public user object
+    const user = req.session.user || { name: 'Public', image: 'default.png' };
+
+    // Pass the organized topics, posts, and user to the EJS template
+    res.render('topics', { topicsWithPosts: Object.values(topicsWithPosts), user });
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
