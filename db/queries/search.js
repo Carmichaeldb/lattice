d0933f451703d86c468ff18a2fa51acf16fe4a65
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-03 12:01:31 -0800

    implement the getSearch method to get result from db based on the given search value

--- db/queries/search.js
+++ db/queries/search.js
@@ -1,6 +1,16 @@
+const db = require('../connection');
 
 const getSearch = function (searchText) {
   console.log("inside get search func");
+  return db.query(`SELECT posts.id, topics.topic_name AS topic, title, description FROM posts
+  JOIN topics ON topic_id = topics.id where topic_name LIKE $1;`, [`%${searchText}%`])
+    .then((result) => {
+      console.log("result.rows:", result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log("Error:", err.message);
+    });
 };
 
 module.exports = { getSearch };
