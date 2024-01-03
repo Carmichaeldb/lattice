c92a2dd749e52912b3a775c80f323f781588afab
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-03 13:15:34 -0800

    add url column name in the select query and add 2 conditions in where clause using OR operator

--- db/queries/search.js
+++ db/queries/search.js
@@ -1,9 +1,9 @@
 const db = require('../connection');
 
 const getSearch = function (searchText) {
-  console.log("inside get search func");
-  return db.query(`SELECT posts.id, topics.topic_name AS topic, title, description FROM posts
-  JOIN topics ON topic_id = topics.id where topic_name LIKE $1;`, [`%${searchText}%`])
+  console.log("inside get search func:::searchText::", searchText);
+  return db.query(`SELECT posts.id, topics.topic_name AS topic, url, title, description FROM posts
+  JOIN topics ON topic_id = topics.id where (topic_name LIKE $1) OR (title LIKE $1);`, [`%${searchText}%`])
     .then((result) => {
       console.log("result.rows:", result.rows);
       return result.rows;
