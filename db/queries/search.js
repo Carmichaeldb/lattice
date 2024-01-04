f964e2f85fac8ebab12af8fbc91b6b8b5d8d17f1
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-03 22:46:35 -0800

    pressed enter

--- db/queries/search.js
+++ db/queries/search.js
@@ -7,7 +7,8 @@ const getSearch = function (searchText) {
   searchText = searchText.toLowerCase();
   console.log(":searchText 0::", searchText);
   return db.query(`SELECT posts.id, topics.topic_name AS topic, url, title, description FROM posts
-  JOIN topics ON topic_id = topics.id where (LOWER(topic_name) LIKE $1) OR (LOWER(title) LIKE $1) OR (LOWER(description) LIKE $1);`, [`%${searchText}%`])
+  JOIN topics ON topic_id = topics.id where (LOWER(topic_name) LIKE $1) OR (LOWER(title) LIKE $1)
+  OR (LOWER(description) LIKE $1);`, [`%${searchText}%`])
     .then((result) => {
       console.log("result.rows:", result.rows);
       return result.rows;
