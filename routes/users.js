8f2ea1fae8ce245782dbdf576e0459e7ce51cc8e
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-05 15:24:32 -0800

    add get request for the endpoint /users/new for create button to render create new post form and add post request for endpoint /users/new for submit button to make entries of new post created in db by calling insertNewPostByUser func and to redirect it to created post

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,39 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+
+const { insertNewPostByUser } = require('../db/queries/users');
 
 //localhost:8080/users/ is the endpoint bz we tell app to use /users as start for this file i.e app.use('/users', usersRoutes);
 router.get('/', (req, res) => {
   res.render('users');
 });
+
+//for create button to render create new post form
+router.get('/new', (req, res) => {
+  console.log("inside get users/new:");
+  res.render('post');
+});
+
+//for submit button to make entries of new post created in db and to redirect it to created post
+router.post('/new', (req, res) => {
+  const title = req.body.title;
+  const topic = req.body.topic;
+  const url = req.body.url;
+  const description = req.body.description;
+  const author = req.body.author;
+  insertNewPostByUser(title, topic, url, description, author)
+    .then((resultPostId) => {
+      console.log("resultId::", resultPostId)
+      //const templateVars = { searchResult };
+      res.redirect("/users/" + resultPostId);
+    })
+    .catch((err) => {
+      console.log("Error:", err.message);
+    });
+});
+
+
 
 module.exports = router;
