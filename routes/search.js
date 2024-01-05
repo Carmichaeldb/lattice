938b6d460cc49ba5b57808c63838bec10de391db
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-05 15:15:34 -0800

    comment is added

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// Queries
 const { getSearch } = require('../db/queries/search');
 
-// Home page
 router.post('/', (req, res) => {
   console.log("inside router");
-  const searchText = req.body.search; //name of the input field
+  const searchText = req.body.search; //search is name of the input field
   getSearch(searchText)
     .then((searchResult) => {
       console.log("searchResult::", searchResult)
