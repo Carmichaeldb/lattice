0f5fe31a99e8ccfb5fd0cf0610829bd227ad936a
Roderick Shoolbraid <rdxenv@gmail.com>
2024-01-09 12:43:07 -0800

    topics route

--- server.js
+++ server.js
@@ -39,11 +39,23 @@ const usersRoutes = require('./routes/users');
 const searchRoutes = require('./routes/search');
 const postsRoutes = require('./routes/posts');
 
+
+/////////////// >>>>> new code
+const topicsRoutes = require('./routes/topics');///topic route
+
+
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/', postsRoutes);
 app.use('/', usersRoutes);
+
+
+
+/////////////// >>>>> new code
+app.use('/topics', topicsRoutes);
+
+
 app.use('/search', searchRoutes);
 
 app.listen(PORT, () => {
