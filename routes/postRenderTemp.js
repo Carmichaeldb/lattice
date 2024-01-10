012e4ad635049429e4548b8a2e2cb082382d7fe7
Roderick Shoolbraid <rdxenv@gmail.com>
2024-01-10 10:58:12 -0800

    comment and rating routes

--- routes/postRenderTemp.js
+++ routes/postRenderTemp.js
@@ -5,19 +5,26 @@ const router = express.Router();
 
 const { getPostById } = require('../db/queries/postQueries');
 const { getCommentsByPostId } = require('../db/queries/commentQuery'); // Import the function to get comments
+const { getAverageRating } = require('../db/queries/ratingQuery'); // Import the function to get average rating
 
 router.get('/posts/:postId', (req, res) => {
   const postId = req.params.postId;
 
-  // Use Promise.all to fetch both the post and its comments concurrently
+  // Use Promise.all to fetch the post, its comments, and its average rating concurrently
   Promise.all([
     getPostById(postId),
-    getCommentsByPostId(postId)
+    getCommentsByPostId(postId),
+    getAverageRating(postId) // Fetch the average rating
   ])
-    .then(([post, comments]) => {
+    .then(([post, comments, rating]) => {
       if (post) {
-      // Render the post along with its comments
-        res.render('postRender', { post: post, comments: comments, user: req.session.user });
+        // Render the post along with its comments and rating
+        res.render('postRender', { 
+          post: post, 
+          comments: comments, 
+          rating: rating, // Pass the average rating to the view
+          user: req.session.user 
+        });
       } else {
         res.status(404).send('Post not found');
       }
