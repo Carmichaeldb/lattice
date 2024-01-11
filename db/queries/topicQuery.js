06be81acdd613d5338905c530b648c4cade6b3a5
David Carmichael <carmichael.db88@gmail.com>
2024-01-11 02:38:02 +0000

    Edit: refactored code

--- db/queries/topicQuery.js
+++ db/queries/topicQuery.js
@@ -9,13 +9,4 @@ const getAllTopics = () => {
     });
 };
 
-const getPostsByTopicId = (topicId) => {
-  return db.query('SELECT * FROM posts WHERE topic_id = $1 ORDER BY created_at DESC', [topicId])
-    .then(result => result.rows)
-    .catch(err => {
-      console.error('Error executing query', err.stack);
-      throw err;
-    });
-};
-
-module.exports = { getAllTopics, getPostsByTopicId };
+module.exports = { getAllTopics };
--- routes/topics.js
+++ routes/topics.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getAllTopics, getPostsByTopicId } = require('../db/queries/topicQuery');
+const { getAllTopics } = require('../db/queries/topicQuery');
 
 router.get('/', async (req, res) => {
   try {
-    const topicsResult = await getAllTopics();
-    const topicsWithPosts = {};
-
-    for (const topic of topicsResult) {
-      const postsResult = await getPostsByTopicId(topic.id);
-      topicsWithPosts[topic.id] = {
-        ...topic,
-        posts: postsResult
-      };
-    }
-
-    // Pass the organized topics, posts, and user to the EJS template
-    res.render('topic', { topicsWithPosts: Object.values(topicsWithPosts)});
+    const topics = await getAllTopics();
+    const templateVars = { topics };
+    // Pass the topics to the EJS template
+    res.render('topic', templateVars);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
