b9cf232e68a76182a6c91200697a67ee2ca8e1bc
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-11 08:45:24 -0800

    implement get method for endpoints /createdPosts,/likedPosts,/ratedPosts

--- routes/posts.js
+++ routes/posts.js
@@ -9,7 +9,7 @@ const express = require('express');
 const router = express.Router();
 
 // Queries
-const { getAllPosts } = require('../db/queries/allPosts');
+const { getAllPosts, getUserPosts, getUserLikedPosts, getUserRatedPosts } = require('../db/queries/allPosts');
 const { getUser } = require("../db/queries/users.js");
 
 // Home page
@@ -21,7 +21,62 @@ router.get('/', (req, res) => {
       console.log("user::", user);
       const userId = user[0].id;
       const username = user[0].username;
-      const templateVars = { posts, userId, username };
+      const email = user[0].email;
+      const templateVars = { posts, userId, username, email };
+      res.render('index', templateVars);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+// displays user created posts
+router.get('/createdPosts', (req, res) => {
+  console.log("inside get createdPosts ,posts.js::");
+  const userId = req.session["userId"];
+  Promise.all([getUserPosts(userId), getUser(userId)])
+    .then(([posts, user]) => {
+      console.log("user::", user);
+      const userId = user[0].id;
+      const username = user[0].username;
+      const email = user[0].email;
+      const templateVars = { posts, userId, username, email };
+      res.render('userDetails', templateVars);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+// displays user liked posts
+router.get('/likedPosts', (req, res) => {
+  console.log("inside get likedPosts ,posts.js::");
+  const userId = req.session["userId"];
+  Promise.all([getUserLikedPosts(userId), getUser(userId)])
+    .then(([posts, user]) => {
+      console.log("user::", user);
+      const userId = user[0].id;
+      const username = user[0].username;
+      const email = user[0].email;
+      const templateVars = { posts, userId, username, email };
+      res.render('index', templateVars);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+// displays user rated posts
+router.get('/ratedPosts', (req, res) => {
+  console.log("inside get ratedPosts ,posts.js::");
+  const userId = req.session["userId"];
+  Promise.all([getUserRatedPosts(userId), getUser(userId)])
+    .then(([posts, user]) => {
+      console.log("user::", user);
+      const userId = user[0].id;
+      const username = user[0].username;
+      const email = user[0].email;
+      const templateVars = { posts, userId, username, email };
       res.render('index', templateVars);
     })
     .catch(err => {
