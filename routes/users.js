fc9ecb257dad97a9bc0dad0752ff19162657c4f1
David Carmichael <carmichael.db88@gmail.com>
2024-01-11 00:10:57 +0000

    Edit: Routes/users move /users/:userid to bottom, Edit: view/nav change new post route back to /users/new Edit: views/post add some top padding to post title and likes/reviews elements

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,43 @@ const router = express.Router();
 const { insertNewPostByUser } = require('../db/queries/users');
 const { checkUser } = require("../db/queries/users.js");
 
+// logins in user
+router.get('/login', (req, res) => {
+  // sets session-cookie to userId 1
+  req.session.userId = 1;
+  // redirects to index page
+  res.redirect("/");
+});
+
+// logs out user
+router.get('/logout', (req, res) => {
+  req.session = null;
+  res.redirect("/");
+});
+
+//for create button to render create new post form
+router.get('/users/new', (req, res) => {
+  console.log("inside get users/new:");
+  res.render('newPost');
+});
+
+//for submit button to make entries of new post created in db and to redirect it to created post
+router.post('/new', (req, res) => {
+  const title = req.body.title;
+  const topic = req.body.topic;
+  const url = req.body.url;
+  const description = req.body.description;
+  const author = req.body.author;
+  insertNewPostByUser(title, topic, url, description, author)
+    .then((resultPostId) => {
+      console.log("resultId::", resultPostId)
+      res.redirect("/posts/" + resultPostId);  // will display specific post
+    })
+    .catch((err) => {
+      console.log("Error:", err.message);
+    });
+});
+
 // render users page
 router.get('/users/:userid', (req, res) => {
   const user = req.params.userid;
@@ -30,43 +67,6 @@ router.get('/users/:userid', (req, res) => {
         // if user does not exist (false) redirect to login
         res.redirect("/login");
       }
-    });
-});
-
-// logins in user
-router.get('/login', (req, res) => {
-  // sets session-cookie to userId 1
-  req.session.userId = 1;
-  // redirects to index page
-  res.redirect("/");
-});
-
-// logs out user
-router.get('/logout', (req, res) => {
-  req.session = null;
-  res.redirect("/");
-});
-
-//for create button to render create new post form
-router.get('/new', (req, res) => {
-  console.log("inside get users/new:");
-  res.render('newPost');
-});
-
-//for submit button to make entries of new post created in db and to redirect it to created post
-router.post('/new', (req, res) => {
-  const title = req.body.title;
-  const topic = req.body.topic;
-  const url = req.body.url;
-  const description = req.body.description;
-  const author = req.body.author;
-  insertNewPostByUser(title, topic, url, description, author)
-    .then((resultPostId) => {
-      console.log("resultId::", resultPostId)
-      res.redirect("/posts/" + resultPostId);  // will display specific post
-    })
-    .catch((err) => {
-      console.log("Error:", err.message);
     });
 });
 
