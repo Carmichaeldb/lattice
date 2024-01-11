c931e1fccf6196e15aee372c209f4bcecf798a0e
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-11 08:29:48 -0800

    implement getUserPosts, getUserLikedPosts, getUserRatedPosts to fetch single user created posts, liked posts, Rated posts respectively using userId as argument

--- db/queries/allPosts.js
+++ db/queries/allPosts.js
@@ -8,4 +8,38 @@ const getAllPosts = () => {
     });
 };
 
-module.exports = { getAllPosts };
+const getUserPosts = (userId) => {
+  console.log("inside getUserPosts");
+  return db.query(`SELECT posts.id, url, topics.topic_name AS topic, title, description FROM posts
+  JOIN topics ON topic_id = topics.id where posts.user_id=$1;`, [userId])
+    .then(data => {
+      console.log(" data.rows::", data.rows);
+      return data.rows;
+    });
+};
+
+const getUserLikedPosts = (userId) => {
+  console.log("inside getUserLikedPosts");
+  return db.query(`SELECT posts.id, url, topics.topic_name AS topic, title, description FROM posts
+  JOIN topics ON topic_id = topics.id
+  JOIN post_likes ON posts.id =post_id where post_likes.user_id =$1;`, [userId])
+    .then(data => {
+      console.log(" data.rows::", data.rows);
+      return data.rows;
+    });
+};
+
+
+const getUserRatedPosts = (userId) => {
+  console.log("inside getUserRatedPosts");
+  return db.query(`SELECT posts.id, url, topics.topic_name AS topic, title, description FROM posts
+  JOIN topics ON topic_id = topics.id
+  JOIN post_ratings ON posts.id =post_id where post_ratings.user_id =$1;`, [userId])
+    .then(data => {
+      console.log(" data.rows::", data.rows);
+      return data.rows;
+    });
+};
+
+
+module.exports = { getAllPosts, getUserPosts, getUserLikedPosts, getUserRatedPosts };
