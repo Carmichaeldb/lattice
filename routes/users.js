98f49dbcd40ff627a4e699362c7c224cb036530c
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-11 23:29:17 -0800

    fix conflicts

--- routes/users.js
+++ routes/users.js
@@ -27,89 +27,88 @@ router.get('/logout', (req, res) => {
 
 //for create button to render create new post form
 router.get('/users/new', (req, res) => {
-  router.get('/users/new', (req, res) => {
-    console.log("inside get users/new:");
-    const userId = req.session["userId"];
-    getUser(userId)
-      .then((user) => {
-        console.log("user::", user);
-        const userId = user[0].id;
-        const username = user[0].username;
-        const email = user[0].email;
-        const templateVars = { userId, username, email };
-        res.render('newPost', templateVars);
-      });
-  });
+  console.log("inside get users/new:");
+  const userId = req.session["userId"];
+  getUser(userId)
+    .then((user) => {
+      console.log("user::", user);
+      const userId = user[0].id;
+      const username = user[0].username;
+      const email = user[0].email;
+      const templateVars = { userId, username, email };
+      res.render('newPost', templateVars);
+    });
+});
 
-  //for submit button to make entries of new post created in db and to redirect it to created post
-  router.post('/users/new', (req, res) => {
-    const title = req.body.title;
-    const topic = req.body.topic;
-    const url = req.body.url;
-    const description = req.body.description;
-    const author = req.body.author;
-    insertNewPostByUser(title, topic, url, description, author)
-      .then((resultPostId) => {
-        console.log("resultId::", resultPostId)
-        res.redirect("/posts/" + resultPostId);  // will display specific post
-      })
-      .catch((err) => {
-        console.log("Error:", err.message);
-      });
-  });
+//for submit button to make entries of new post created in db and to redirect it to created post
+router.post('/users/new', (req, res) => {
+  const title = req.body.title;
+  const topic = req.body.topic;
+  const url = req.body.url;
+  const description = req.body.description;
+  const author = req.body.author;
+  insertNewPostByUser(title, topic, url, description, author)
+    .then((resultPostId) => {
+      console.log("resultId::", resultPostId)
+      res.redirect("/posts/" + resultPostId);  // will display specific post
+    })
+    .catch((err) => {
+      console.log("Error:", err.message);
+    });
+});
 
-  // render user profile page
-  router.get('/users/:userid', (req, res) => {
-    const user = req.params.userid;
-    const userId = req.session["userId"];
-    // Checks if user exists
-    checkUser(userId)
-      .then((userFound) => {
-        if (userFound) {
-          if (user != userId) {
-            console.log("inside checkUser");
-            res.status(401).send("Error 401: Unauthorized Access. This profile does not belong to you.");
-            return;
-          }
-          // if user exists (true) render users view
-          Promise.all([getUserPosts(userId), getUserLikedPosts(userId), getUser(userId)])
-            .then(([posts, posts1, user]) => {
-              console.log("user::", user);
-              const userId = user[0].id;
-              const username = user[0].username;
-              const email = user[0].email;
-              const templateVars = { posts, posts1, userId, username, email };
-              res.render("userDetails", templateVars);
-            })
-            .catch((err) => {
-              console.log("Error:", err.message);
-            });
-
-        } else {
-          // if user does not exist (false) redirect to login
-          res.redirect("/login");
+// render user profile page
+router.get('/users/:userid', (req, res) => {
+  const user = req.params.userid;
+  const userId = req.session["userId"];
+  // Checks if user exists
+  checkUser(userId)
+    .then((userFound) => {
+      if (userFound) {
+        if (user != userId) {
+          console.log("inside checkUser");
+          res.status(401).send("Error 401: Unauthorized Access. This profile does not belong to you.");
+          return;
         }
-      });
-  });
+        // if user exists (true) render users view
+        Promise.all([getUserPosts(userId), getUserLikedPosts(userId), getUser(userId)])
+          .then(([posts, posts1, user]) => {
+            console.log("user::", user);
+            const userId = user[0].id;
+            const username = user[0].username;
+            const email = user[0].email;
+            const templateVars = { posts, posts1, userId, username, email };
+            res.render("userDetails", templateVars);
+          })
+          .catch((err) => {
+            console.log("Error:", err.message);
+          });
 
-  //to save or update user Details in db
-  router.post('/users/:userid', (req, res) => { //save button
-    console.log("inside post /users/:userid");
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const userId = req.session["userId"];
-    updateUserDetails(username, email, password, firstName, lastName, userId)
-      .then((userDetails) => {
-        console.log("userDetails::", userDetails);
-        // const userId = userDetails[0].id;
-        res.redirect("/users/" + userId);
-      })
-      .catch((err) => {
-        console.log("Error:", err.message);
-      });
-  });
+      } else {
+        // if user does not exist (false) redirect to login
+        res.redirect("/login");
+      }
+    });
+});
 
-  module.exports = router;
+//to save or update user Details in db
+router.post('/users/:userid', (req, res) => { //save button
+  console.log("inside post /users/:userid");
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const userId = req.session["userId"];
+  updateUserDetails(username, email, password, firstName, lastName, userId)
+    .then((userDetails) => {
+      console.log("userDetails::", userDetails);
+      // const userId = userDetails[0].id;
+      res.redirect("/users/" + userId);
+    })
+    .catch((err) => {
+      console.log("Error:", err.message);
+    });
+});
+
+module.exports = router;
