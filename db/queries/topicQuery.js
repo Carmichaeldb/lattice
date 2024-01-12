3bfd4e8e9f2be314a46a076620708ed1a458a38a
Roderick Shoolbraid <rdxenv@gmail.com>
2024-01-12 13:48:01 -0800

    topic ONLY filter for search

--- db/queries/topicQuery.js
+++ db/queries/topicQuery.js
@@ -1,13 +1,33 @@
 const db = require('../connection');
 
-const getAllTopics = () => {
+// Modified getAllTopics to accept a search term and filter topics accordingly
+const getAllTopics = (searchTerm) => {
   console.log("in query");
-  return db.query('SELECT * FROM topics')
-    .then(result => result.rows)
-    .catch(err => {
-      console.error('Error executing query', err.stack);
-      throw err;
-    });
+  
+  // Check if searchTerm is provided
+  if (searchTerm) {
+    // If searchTerm is provided, filter topics based on the searchTerm
+    const formattedSearchTerm = `%${searchTerm.toLowerCase()}%`;
+    return db.query(`
+      SELECT posts.id, topics.topic_name AS topic, url, title, description 
+      FROM posts
+      JOIN topics ON posts.topic_id = topics.id 
+      WHERE LOWER(topics.topic_name) LIKE $1;
+    `, [formattedSearchTerm])
+      .then(result => result.rows)
+      .catch(err => {
+        console.error('Error executing query', err.stack);
+        throw err;
+      });
+  } else {
+    // If no searchTerm is provided, return all topics as before
+    return db.query('SELECT * FROM topics')
+      .then(result => result.rows)
+      .catch(err => {
+        console.error('Error executing query', err.stack);
+        throw err;
+      });
+  }
 };
 
 module.exports = { getAllTopics };
